c85f77f60d4103bfe6d19bf6addd137b598200d7
manviiiichawlaa <manvi25csu139@ncuindia.edu>
2026-01-21 13:56:32 +0530

    Add files via upload

---
+++ ques2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r;
+    std::cout<<"enter the value of r";
+    std::cin>>r;
+    float area=(3.14*r*r);
+    std::cout<<"the value of area is"<<area;
+    return 0;
+
+}
